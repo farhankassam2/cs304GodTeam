0dc83844f57168daee72f160e72a32cd00b3ce6d
Siddhartha Gupta <w7h0b@USE.CS.UBC.CA>
2018-06-17 21:37:47 -0700

    Changed the reference in Admin for Customer Queyr.Function Key

--- src/restaurantapp/Admin.java
+++ src/restaurantapp/Admin.java
@@ -274,13 +274,13 @@ public class Admin extends javax.swing.JFrame {
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
-        setContentPane(new StoreQuery());
+        setContentPane(new StoreQuery(this.db,this));
         pack();
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
-        setContentPane(new CustomerQuery());
+        setContentPane(new CustomerQuery(this.db,this));
         pack();
     }//GEN-LAST:event_jButton5ActionPerformed
 
